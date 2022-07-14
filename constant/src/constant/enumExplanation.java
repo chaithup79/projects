a83751820bc08979e979fb9dd06013034be08da9
chaithup79 <chaitanya.parameshwara@gmail.com>
2022-07-14 12:13:11 +0530

    enum

---
+++ constant/src/constant/enumExplanation.java
@@ -0,0 +1,18 @@
+package constant;
+
+public class enumExplanation {
+
+	public enum name
+	{
+		CHAI,
+		HEHE,
+		SHARU,
+		CLASSY
+	}
+	
+	public static void main(String[] args) {
+		System.out.println(name.CHAI);
+		System.out.println(name.HEHE);
+	}
+	
+}
